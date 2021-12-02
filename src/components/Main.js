731a47eaab31568463c3b4108caef84e878ae8fe
Mahmoud <mahmoudabdulmuty@gmail.com>
2021-12-02 07:31:56 +0200

    adding Card Component

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,19 +9,22 @@ export default function Main() {
 					<img className="card__img" src={mountFuji} alt="mount-fuji" />
 				</div>
 				<div className="card__details">
-					<div class="card__location">
+					<div className="card__location">
 						<FaMapMarkerAlt className="card__icon" />
-						<div className="card__country">japan</div>
-						<div class="card__maps">View on Google Maps</div>
+						<span className="card__country">japan</span>
+						<a href="/" className="card__maps">
+							View on Google Maps
+						</a>
 					</div>
 					<div className="card__title">Mount Fuji</div>
-					<div class="card__date">12 Jan, 2021 - 24 Jan, 2021</div>
+					<div className="card__date">12 Jan, 2021 - 24 Jan, 2021</div>
 					<div className="card__description">
 						Mount Fuji is the tallest mountain in Japan, standing at 3,776
 						meters (12,380 feet). Mount Fuji is the single most popular tourist
 						site in Japan, for both Japanese and foreign tourists.
 					</div>
 				</div>
+				<hr className="hr-line" />
 			</div>
 		</div>
 	);
